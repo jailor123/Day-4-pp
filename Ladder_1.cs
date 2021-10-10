a63d3f36ead8c71e2f0de385cbf6ba63186b59b3
jailor123 <91145870+jailor123@users.noreply.github.com>
2021-10-10 22:38:41 +0530

    single_player.cs

---
+++ Ladder_1.cs
@@ -0,0 +1,12 @@
+static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the game of snake and ladder");
+            Console.WriteLine();
+            Console.WriteLine(" Player1 mode: Starting position 0");  //as per UC1
+            Console.WriteLine("Game starts");
+            int position = 0;
+            int dice;
+
+        }
+    }
+}
